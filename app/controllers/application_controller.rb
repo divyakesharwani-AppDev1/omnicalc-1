a73385a6b25cb00e789dc97a003ccf533b7428ca
divyakesharwani <dkesharw@chicagobooth.edu>
2022-01-30 05:43:47 +0000

    random number form and results workign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,17 @@ class ApplicationController < ActionController::Base
     render({:template=>"calculation_templates/square_results.html.erb"})
   end
 
+  def blank_random_form
+    render({:template=>"calculation_templates/random_form.html.erb"})
+  end
+
+
   def calculate_random
     #params = {"user_min"=>"1.5", "user_max"=>"4.5"}
 
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
-    @random_results = rand(@lower..@upper)
-    
+    @random_results = rand(@lower..@upper)   
 
 
     render({:template=>"calculation_templates/rand_results.html.erb"})
